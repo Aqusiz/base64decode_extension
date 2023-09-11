d566de3294f18668f2efbfbbc720aa13fb5d2d1a
Juno <kjo1797@gmail.com>
2023-09-11 15:58:09 +0900

    Add converting

--- content.js
+++ content.js
@@ -1,17 +1,18 @@
 const base64RE = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
 
 function isBase64(text) {
-    console.log(text);
-    console.log(base64RE.test(text));
     return base64RE.test(text);
 }
 
-document.addEventListener('selectionchange', () => {
-    let selectedText = window.getSelection().toString().trim();
-    if(isBase64(selectedText)) {
-        chrome.runtime.sendMessage({
-            request: 'updateContextMenu',
-            selection: selectedText
-        })
+const pList = document.querySelectorAll("p");
+for(let p of pList) {
+    const text = p.textContent;
+    if(isBase64(text)) {
+        let link = document.createElement("a");
+        link.innerText = text;
+        link.href = atob(text);
+        link.target = "_blank";
+
+        p.parentNode.replaceChild(link, p);
     }
-});
+}
