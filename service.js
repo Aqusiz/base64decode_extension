d4e6a4c8751482b7919072ab38d9bb2eccde5d58
Juno <kjo1797@gmail.com>
2024-09-13 07:42:04 +0900

    feat decode twice menu

--- service.js
+++ service.js
@@ -1,5 +1,7 @@
-const cmId = "base64decode";
+const cmId = "context1";
+const cmId2 = "context2";
 const cmTitle = "복호화하기";
+const cmTitle2 = "2번 복호화하기";
 
 function onClickMenu(info) {
     const text = info.selectionText;
@@ -8,16 +10,28 @@ function onClickMenu(info) {
     chrome.tabs.create({url: decodedText});
 };
 
+function onClickMenu2(info) {
+    const text = info.selectionText;
+    const decodedTwiceText = atob(atob(text));
+
+    chrome.tabs.create({url: decodedTwiceText});
+}
+
 chrome.contextMenus.removeAll(() => {
     chrome.contextMenus.create({
         id: cmId,
         title: cmTitle,
         contexts: ["selection"]
-    }, () => {
-        if(chrome.runtime.lastError) {
-            
-        }
+    });
+
+    chrome.contextMenus.create({
+        id: cmId2,
+        title: cmTitle2,
+        contexts: ["selection"]
     });
 });
 
-chrome.contextMenus.onClicked.addListener(onClickMenu);
+chrome.contextMenus.onClicked.addListener((info) => {
+    if(info.menuItemId === cmId) onClickMenu(info);
+    if(info.menuItemId == cmId2) onClickMenu2(info);
+});
